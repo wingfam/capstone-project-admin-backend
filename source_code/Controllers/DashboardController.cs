2009fac2c09b5d399c311a6b49effed131b7768e
hantomi <redragon12371@gmail.com>
2023-07-25 11:01:22 +0700

    Include Dashboard get data

---
+++ source_code/Controllers/DashboardController.cs
@@ -0,0 +1,100 @@
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace DeliverBox_BE.Controllers
+{
+    public class DashboardController : Controller
+    {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "QY1XtCBtW6LdNwMGx36VwjJKJqKYJmNOlP30jaxP",
+            BasePath = "https://slsd-capstone-project-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        };
+        IFirebaseClient client;
+
+        [HttpGet(template: "get-total-resident")]
+        public ActionResult GetTotalResident ()
+        {
+            try
+            {
+                client = new FireSharp.FirebaseClient (config);
+                FirebaseResponse response = client.Get("Resident");
+
+                int count = 0;
+                dynamic data = JsonConvert.DeserializeObject<dynamic>(response.Body);
+                foreach (var item in data )
+                {
+                    count ++; //Count the number  of resident in the data list
+                }
+                var result = new {obj = "Total Resident", count = count};
+
+                var json = JsonConvert.SerializeObject(result, Formatting.Indented, new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.None });
+                return Content(json, "application/json");
+            }
+            catch (Exception ex)
+            {
+                var result = new { errCode = 1, errMessage = ex.Message };
+                var json = JsonConvert.SerializeObject(result, Formatting.Indented, new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.None });
+                return Content(json, "application/json");
+            }
+        }
+
+        [HttpGet(template: "get-total-box")]
+        public ActionResult GetTotalBox()
+        {
+            try
+            {
+                client = new FireSharp.FirebaseClient(config);
+                FirebaseResponse response = client.Get("Box");
+
+                int count = 0;
+                dynamic data = JsonConvert.DeserializeObject<dynamic>(response.Body);
+                foreach (var item in data)
+                {
+                    count++; //Count the number  of box in the data list
+                }
+                var result = new { obj = "Total Box", count = count };
+
+                var json = JsonConvert.SerializeObject(result, Formatting.Indented, new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.None });
+                return Content(json, "application/json");
+            }
+            catch (Exception ex)
+            {
+                var result = new { errCode = 1, errMessage = ex.Message };
+                var json = JsonConvert.SerializeObject(result, Formatting.Indented, new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.None });
+                return Content(json, "application/json");
+            }
+        }
+
+        [HttpGet(template: "get-total-order")]
+        public ActionResult GetTotalOrder()
+        {
+            try
+            {
+                client = new FireSharp.FirebaseClient(config);
+                FirebaseResponse response = client.Get("BookingOrder");
+
+                int count = 0;
+                dynamic data = JsonConvert.DeserializeObject<dynamic>(response.Body);
+                foreach (var item in data)
+                {
+                    count++; //Count the number  of booking order in the data list
+                }
+                var result = new { obj = "Total Order", count = count };
+
+                var json = JsonConvert.SerializeObject(result, Formatting.Indented, new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.None });
+                return Content(json, "application/json");
+            }
+            catch (Exception ex)
+            {
+                var result = new { errCode = 1, errMessage = ex.Message };
+                var json = JsonConvert.SerializeObject(result, Formatting.Indented, new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.None });
+                return Content(json, "application/json");
+            }
+        }
+    }
+}
