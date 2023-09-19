7ed3ff67dd7a663377e20ddb5d3d109a657272f6
Nguyen Huynh Nhat Minh <66111145+wingfam@users.noreply.github.com>
2023-09-19 10:02:36 +0700

    update customer controller

--- source_code/Models/BookingHistoryResponseModel.cs
+++ source_code/Models/BookingHistoryResponseModel.cs
@@ -3,6 +3,7 @@
     public class BookingHistoryResponseModel
     {
         public string? BoxName { get; set; }
+        public string? Location { get; set; }
         public string? ValidDate { get; set; }
         public string? CreateDate { get; set; }
         public int? Status { get; set; }
