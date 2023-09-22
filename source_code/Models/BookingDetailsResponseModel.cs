d4f4e6a0e1dd5210005e7cabccc24c501d563195
Nguyen Huynh Nhat Minh <66111145+wingfam@users.noreply.github.com>
2023-09-22 15:42:16 +0700

    update customer controller
    - update active booking model
    - update booking history response model

---
+++ source_code/Models/BookingDetailsResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DeliverBox_BE.Models
+{
+    public class BookingDetailsResponseModel
+    {
+        public string? location { get; set; }
+        public string? bookingCode { get; set; }
+        public string? unlockCode { get; set; }
+    }
+}
