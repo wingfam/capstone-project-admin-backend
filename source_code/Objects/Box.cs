4922d459aebd7b464905ca0d144ac248bc01cf3e
Nguyen Huynh Nhat Minh <66111145+wingfam@users.noreply.github.com>
2023-09-23 08:17:14 +0700

    update and add
    - update Box model, BoxController, DashboardController,
    - add BusinessChart, BusinessChartObjects

--- source_code/Objects/Box.cs
+++ source_code/Objects/Box.cs
@@ -11,16 +11,18 @@ public partial class Box
     {
     }
 
-    public Box(string name, int? status, string cabinetId)
+    public Box(string name, int? status, int? process, string cabinetId)
     {
         this.nameBox = name;
         this.status = status;
+        this.process = process;
         this.cabinetId = cabinetId;
     }
 
     public string? id { get; set; } = null!;
     public string? nameBox { get; set; }
     public int? status { get; set; }
+    public int? process { get; set; }
     public string? cabinetId { get; set; }
 
     [JsonIgnore]
--- source_code/Objects/BusniessChartObject.cs
+++ source_code/Objects/BusniessChartObject.cs
@@ -1,22 +1,20 @@
 ﻿namespace DeliverBox_BE.Objects
 {
-    public class BusniessChartObject
+    public class BusinessChartObject
     {
-        public BusniessChartObject(string? id, string? date, string? business_1_name,
-            int? busniess_1_amount, string? business_2_name, int? business_2_amount)
+        public BusinessChartObject(string? date, string? businessName,
+            int? busniessAmount)
         {
-            this.id = id;
+            id = Interlocked.Increment(ref nextId);
             this.date = date;
-            this.business_1_name = business_1_name;
-            this.business_2_name = business_2_name;
-            this.business_1_amount = busniess_1_amount;
-            this.business_2_amount = business_2_amount;
+            this.businessName = businessName;
+            this.businessAmount = busniessAmount;
         }
-        public string? id { get; set; }
+
+        static int nextId;
+        public int id { get; private set; }
         public string? date { get; set; }
-        public string? business_1_name { get; set; }
-        public int? business_1_amount { get; set; }
-        public string? business_2_name { get; set; }
-        public int? business_2_amount { get; set; }
+        public string? businessName { get; set; }
+        public int? businessAmount { get; set; }
     }
 }
